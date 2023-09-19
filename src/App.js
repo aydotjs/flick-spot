e4d8862dcee1306620b8400a59c196dce4fc6ede
aydotjs <ayobami66@gmail.com>
2023-09-19 10:29:14 +0100

    bazinga

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import { useState } from "react";
+import { Children, useState } from "react";
 import NavBar from "./NavBar";
 import AllMovies from "./AllMovies";
 import SeenMovies from "./SeenMovies";
 import tempMovieData from "./TempMovieData";
 import tempWatchedData from "./TempWatchedData";
-
 
 const average = (arr) =>
   arr.reduce((acc, cur, _, arr) => acc + cur / arr.length, 0);
@@ -22,7 +21,24 @@ export default function App() {
 
   return (
     <>
-      <NavBar query={query} setQuery={setQuery} movies={movies} />
+      <NavBar>
+        <nav className="nav-bar">
+          <div className="logo">
+            <span role="img">🍿</span>
+            <h1>flick spot</h1>
+          </div>
+          <input
+            className="search"
+            type="text"
+            placeholder="Search movies..."
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+          />
+          <p className="num-results">
+            Found <strong>{movies.length}</strong> results
+          </p>
+        </nav>
+      </NavBar>
       <main className="main">
         <AllMovies isOpen1={isOpen1} setIsOpen1={setIsOpen1} movies={movies} />
         <SeenMovies
--- src/NavBar.js
+++ src/NavBar.js
@@ -1,23 +1,8 @@
 import React from "react";
 
-const NavBar = ({ query, setQuery, movies }) => {
+const NavBar = ({ children }) => {
   return (
-    <nav className="nav-bar">
-      <div className="logo">
-        <span role="img">🍿</span>
-        <h1>flick spot</h1>
-      </div>
-      <input
-        className="search"
-        type="text"
-        placeholder="Search movies..."
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-      />
-      <p className="num-results">
-        Found <strong>{movies.length}</strong> results
-      </p>
-    </nav>
+  {children}
   );
 };
 
